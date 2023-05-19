a38de60d3979e00b7107f8581ba799c01d9923d7
Thomas <toast@sol.at>
2023-05-19 18:06:32 +0200

    Aktualisierung

---
+++ cronjobcommand.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WIFI\JUMMY\Speisen\DbRow;
+
+use WIFI\JUMMY\Mysql;
+// Erlaubt getInstanz (Die Datenbankverbindung) aus Mysql
+
+include "setup.php";
+
+/*
+include 'your_db_connection';
+mysql_query("DELETE FROM your_table_name WHERE Date < NOW()");
+*/
